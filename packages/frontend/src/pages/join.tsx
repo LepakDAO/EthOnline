ec1eedd220368effdd48d42acad4a1cc1f9b1e0c
0xkeivin <keivincatwork@gmail.com>
2022-09-21 23:48:24 +0800

    added NotificationProvider

--- packages/frontend/src/pages/join.tsx
+++ packages/frontend/src/pages/join.tsx
@@ -1,33 +1,35 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import styled from "styled-components";
-import { extractLinearGradientParamsFromTransform } from "@figma-plugin/helpers";
-import Header from "../components/Header";
-import JoinModal from "../components/JoinModal";
-
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styled from 'styled-components'
+import { extractLinearGradientParamsFromTransform } from '@figma-plugin/helpers'
+import Header from '../components/Header'
+import JoinModal from '../components/JoinModal'
+import { NotificationProvider } from '@web3uikit/core'
 const Join: NextPage = () => {
   return (
     <Wrapper>
       <Header />
       <MainContainer>
-        <JoinModal isOpen onClose={() => {}} id="1" />
+        <NotificationProvider>
+          <JoinModal isOpen onClose={() => {}} id="1" />
+        </NotificationProvider>
       </MainContainer>
     </Wrapper>
-  );
-};
+  )
+}
 
 const Wrapper = styled.div`
-  font-family: "Montserrat";
+  font-family: 'Montserrat';
   height: 100vh;
   max-width: 100vw;
   background-color: ${({ theme }) => theme.colors.black};
   color: ${({ theme }) => theme.colors.white};
-`;
+`
 const MainContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-`;
+`
 
-export default Join;
+export default Join
